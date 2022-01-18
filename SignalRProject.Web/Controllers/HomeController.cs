cb9e2ae9e157a418fc11aed700b964b803c9d1ff
burakhayirli <burakhayirli1@hotmail.com>
2022-01-18 12:17:48 +0300

    Add usage hub server into client side

--- SignalRProject.Web/Controllers/HomeController.cs
+++ SignalRProject.Web/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace SignalRProject.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
     }
 }
