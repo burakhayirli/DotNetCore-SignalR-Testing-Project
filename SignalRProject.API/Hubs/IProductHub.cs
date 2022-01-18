60f2fee40319a3bd9bde762609fc15cd0f9ce115
burakhayirli <burakhayirli1@hotmail.com>
2022-01-18 12:01:03 +0300

    Add usage strongly typed hub

---
+++ SignalRProject.API/Hubs/IProductHub.cs
@@ -0,0 +1,13 @@
+﻿using SignalRProject.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRProject.API.Hubs
+{
+    public interface IProductHub
+    {
+        Task ReceiveProduct(Product p);
+    }
+}
